ff50dcecebede8b8876e83057513ed732b3f4bf1
cultivatedvariety <rob.cherry@gmail.com>
2016-04-12 21:00:21 +0100

    initial commit

---
+++ ElementsOfProgrammerInterviews/Chapter01.fs
@@ -0,0 +1,10 @@
+﻿module Chapter01
+
+let ComputeParity num = 
+    let rec ComputeParityRec num parity = 
+        match num with
+        | 0 -> (parity &&& 1)
+        | _ -> ComputeParityRec (num >>> 1) (parity + (num &&& 1))
+
+    ComputeParityRec num 0
+
