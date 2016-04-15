0361bcbcaf30d1c684184957e86f87a9e5141064
cultivatedvariety <rob.cherry@gmail.com>
2016-04-15 15:34:20 +0100

    99 problems - problem 1

---
+++ 99Problems/Lists.fs
@@ -0,0 +1,15 @@
+﻿module Lists
+
+
+
+let FindLastElement01 lst =
+    let rec FindLastElementRec lst =
+        match lst with
+        | [] -> None
+        | [x] -> Some x
+        | _::xs -> FindLastElementRec xs
+
+    FindLastElementRec lst
+
+let FindLastElement02 lst = List.reduce(fun _ x -> x) lst
+    
